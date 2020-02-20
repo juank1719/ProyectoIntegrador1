e95036fe68d087163a7ff2adca8c72e253665377
JUAN CARLOS MUNOZ BOJORQUEZ <juan.munozbj@uanl.edu.mx>
2020-02-19 19:06:37 -0600

    Registro de usuarios creado

---
+++ TaqueriaHernandez/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TaqueriaHernandez.Data;
+
+[assembly: HostingStartup(typeof(TaqueriaHernandez.Areas.Identity.IdentityHostingStartup))]
+namespace TaqueriaHernandez.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
